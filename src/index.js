a710e27d278ce4be93481553d9e3fe84dfee9322
Eric G <baconerie0001@gmail.com>
2023-08-24 19:34:59 +0000

    buncho stuff

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
+import './global.css';
+import styles from './Index.module.css';
+
 export default function MainComponent() {
   return (<>
-    
+    <div><h1>字Button</h1></div>
+
+    <div>
+      <h1>Create a list</h1>
+
+      <ul className={styles.characterList}>
+        <li>lmao</li>
+        <li>lmao</li>
+        <li>lmao</li>
+        <li>lmao</li>
+        <li>lmao</li>
+        <li>lmao</li>
+        <li>lmao</li>
+        <li>lmao</li>
+        <li>lmao</li>
+        <li>lmao</li>
+        <li>lmao</li>
+        <li>lmao</li>
+        <li>lmao</li>
+      </ul>
+    </div>
   </>) 
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     entry: {
-        index: './index.js',
+        index: './src/index.js',
     },
 
     output: {
