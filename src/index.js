a955a873d837ea3deb843f9bb5dab825474a31b3
BaconErie <baconerie0001@gmail.com>
2023-08-25 19:48:13 -0400

    add an upload function

--- src/index.js
+++ src/index.js
@@ -47,6 +47,30 @@ export default function MainComponent() {
     document.body.removeChild(element);
   }
 
+  async function upload() {
+    var input = document.createElement('input');
+    input.type = 'file';
+
+    input.onchange = e => { 
+
+      // getting a hold of the file reference
+      var file = e.target.files[0]; 
+
+      // setting up the reader
+      var reader = new FileReader();
+      reader.readAsText(file,'UTF-8');
+
+      // here we tell the reader what to do when it's done reading...
+      reader.onload = readerEvent => {
+          var content = readerEvent.target.result; // this is the content!
+          console.log( content );
+      }
+
+    }
+
+    input.click();
+  } 
+
   async function handleAddCharacter() {
     setDisableAddChar(true);
 
