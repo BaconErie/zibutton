f46c525945b61cb2787147cf024d1d9f27aeedfd
Eric G <baconerie0001@gmail.com>
2023-08-25 20:49:50 +0000

    buncha stuff

--- src/components/baconerie/PrimaryButton/PrimaryButton.js
+++ src/components/baconerie/PrimaryButton/PrimaryButton.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import styles from 'baconerie/PrimaryButton/PrimaryButton.module.css';
+import styles from './PrimaryButton.module.css';
 
 export default function PrimaryButton({ id, className, onClick, children, disabled }) {
   let overlay = disabled ? null : (<span className={styles.PrimaryButton_overlay}></span>)
--- src/index.js
+++ src/index.js
@@ -4,12 +4,22 @@ import { createRoot } from 'react-dom/client';
 import './global.css';
 import styles from './Index.module.css';
 
+import PrimaryButton from './components/baconerie/PrimaryButton/PrimaryButton';
+
 export default function MainComponent() {
+  function handleAddCharacter() {
+    
+  }
+
   return (<>
     <div><h1>字Button</h1></div>
 
     <div>
       <h1>Create a list</h1>
+      <div class="wrapper">
+        <input id="" type="text" placeholder="Enter a character" />
+        <PrimaryButton onClick={}>Add a character</PrimaryButton>
+      </div>
 
       <ul className={styles.characterList}>
         <li>lmao</li>
