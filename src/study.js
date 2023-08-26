1d44f24fc4b2f446820fac3906b291855e00c8bd
BaconErie <baconerie0001@gmail.com>
2023-08-25 22:11:29 -0400

    Basic layout

--- src/study.js
+++ src/study.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { useState } from 'react';
 
 import './global.css';
 
+import indexStyles from './Index.module.css';
+import styles from './Study.module.css';
+
 export default function StudyPage() {
+  const [ currentDef, setCurrentDef ] = useState('me, myself, I, idek');
+
   return (<>
+    <div><h1><span className={indexStyles.red}>字</span>Button</h1></div>
+
+    <div className={styles.characterDisplayDiv}>
+      <h1>Study Mode</h1>
+      <p>Click the button that shows the next character stroke</p>
+
+      <p class={styles.definition}>Definition: {currentDef}</p>
+      <div class={`${styles.characterDisplay} surfaceDiv`}>
+        beeg div
+      </div>
+    </div>
+
     <div>
-      Hello world!
+      yo wsg whats up gamers
     </div>
   </>)
 }
