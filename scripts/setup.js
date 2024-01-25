14c45974c6935e7c6676ae40c456791d5df8dfd0
BaconErie <baconerie0001@gmail.com>
2024-01-24 20:20:54 -0500

    remove nonfunctional code from setup

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,9 +1,5 @@
 const sqlite3 = require('sqlite3');
 
-console.warn('Warning: Did you remember to change the secret?')
-process.env.DB_PATH = 'dev.db';
-process.env.TOKEN_SECRET = 'secret';
-
 const db = new sqlite3.Database(process.env.DB_PATH);
 
 db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT, time_created INTEGER)');
