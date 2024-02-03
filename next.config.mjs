699c2623eb2dcc17bb3426584008256c64e971e2
BaconErie <baconerie0001@gmail.com>
2024-02-03 18:45:51 -0500

    add allowed origins

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['zibutton.baconerie.com', 'localhost:8000', 'localhost:1024'],
+    },
+  },
+};
 
 export default nextConfig;
